041e4ca583eb2cfe7e053b8991457186ffb1982c
Tornike Shavidze <shavidzet@protonmail.com>
2020-02-28 18:27:47 +0400

    add CRUD feature

--- functions/src/routes.js
+++ functions/src/routes.js
@@ -1,4 +1,4 @@
-// const { get } = require('lodash')
+const { get } = require('lodash')
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
@@ -16,6 +16,55 @@ router.get('/addMessage', async (req, res) => {
   /* eslint-disable */
   const snapshot = await db.ref('/messages').push({ original })
   res.send(original)
+})
+
+// Create
+router.post('/todo', async (req, res) => {
+  const todoName = get(req.body, 'name', '')
+  console.log(req.body.name, 33)
+  if (!todoName) {
+    res.status(400).send('please specify todo name')
+    return
+  }
+  await db.ref('/todos').push({
+    name: todoName
+  })
+  res.send(`Created todo: ${todoName}`)
+})
+
+// Read
+router.get('/todos', async (req, res) => {
+  const todos = await db.ref('/todos').once('value')
+  res.json(todos)
+})
+
+// Update
+router.put('/todo', async (req, res) => {
+  const todoId = get(req.query, 'id', '')
+  if (!todoId) {
+    res.status(400).send('please specify todo id')
+    return
+  }
+  const todoName = get(req.body, 'name', '')
+  if (!todoName) {
+    res.status(400).send('please specify todo name')
+    return
+  }
+  await db.ref(`/todos/${todoId}`).update({
+    name: todoName
+  });
+  res.send(`Update todo name - ${todoId}: ${todoName}`)
+})
+
+// Delete
+router.delete('/todo', async (req, res) => {
+  const todoId = get(req.query, 'id', '')
+  if (!todoId) {
+    res.status(400).send('please specify todo id')
+    return
+  }
+  await db.ref(`/todos/${todoId}`).remove()
+  res.send(`Deleted todo id: ${todoId}`)
 })
 
 module.exports = router
